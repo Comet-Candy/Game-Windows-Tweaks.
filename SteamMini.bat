294f7f3dd40925f017af3cc2c6817d213ae53281
CsmcX <helicopterparkour@gmail.com>
2026-09-04 09:02:49 +1200

    Update SteamMini.bat

--- SteamMini.bat
+++ SteamMini.bat
@@ -1,24 +1,10 @@
 @echo off
-rem DONT USE: -dev -console -cef-disable-sandbox -no-cef-sandbox
-rem -cef-force-32bit breaks launch
+set "ShortcutPath=%USERPROFILE%\Desktop\Steam.lnk"
+set "SteamExe=C:\Program Files (x86)\Steam\steam.exe"
+set "Flags=-nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -disable-winh264 -vrdisable -cef-disable-breakpad -cef-disable-d3d11 -cef-disable-gpu-compositing -cef-disable-gpu -cef-disable-js-logging -cef-disable-occlusion -cef-disable-renderer-restart -noconsole -oldtraymenu -showallbetas"
+set "IconPath=C:\Program Files (x86)\Steam\steam.exe,0"
 
-:: --- Delete old Steam desktop shortcuts ---
-del /q "%USERPROFILE%\Desktop\Steam.lnk" 2>nul
-del /q "%PUBLIC%\Desktop\Steam.lnk" 2>nul
+powershell -NoProfile -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%ShortcutPath%');$s.TargetPath='%SteamExe%';$s.Arguments='%Flags%';$s.WorkingDirectory='C:\Program Files (x86)\Steam';$s.IconLocation='%IconPath%';$s.Save()"
 
-:: --- Create new shortcut with real Steam logo ---
-set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
-set "LNK_PATH=%USERPROFILE%\Desktop\Steam.lnk"
-
-powershell -NoProfile -Command ^
-  "$ws = New-Object -ComObject WScript.Shell; " ^
-  "$s = $ws.CreateShortcut('%LNK_PATH%'); " ^
-  "$s.TargetPath = '%STEAM_PATH%'; " ^
-  "$s.WorkingDirectory = 'C:\Program Files (x86)\Steam'; " ^
-  "$s.IconLocation = '%STEAM_PATH%'; " ^
-  "$s.Description = 'Steam'; " ^
-  "$s.Save()"
-
-:: --- Launch Steam with your flags ---
-cd "C:\Program Files (x86)\Steam"
-start steam.exe -nointro -nobigpicture -nocrashmonitor -disablehighdpi -vrdisable -cef-disable-breakpad -cef-disable-js-logging -noconsole -oldtraymenu -showallbetas -nofriendsui -no-dwrite -nofasthtml -noshaders -no-shared-textures -single_core -disable-winh264 -cef-disable-occlusion -cef-disable-renderer-restart +open steam://open/minigameslist   
+echo Done.
+pause   
